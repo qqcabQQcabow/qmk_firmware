93542f057913c94cea994b3d14c081e4fa5e8e80
Aladin Platon <p.aladin@bk.ru>
2025-07-13 17:25:40 +0300

    fix encoders

--- keyboards/sofle/keymaps/default/config.h
+++ keyboards/sofle/keymaps/default/config.h
@@ -3,7 +3,13 @@
 
 #pragma once
 
+// fix encoder
+#define ENCODER_DIRECTION_FLIP
+#define ENCODER_RESOLUTION 2
+
+// fix unwork right segment
 #define SPLIT_USB_DETECT
+
 #define TRI_LAYER_LOWER_LAYER 2
 #define TRI_LAYER_UPPER_LAYER 3
 #define TRI_LAYER_ADJUST_LAYER 4
